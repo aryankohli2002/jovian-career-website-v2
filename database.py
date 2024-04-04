c90d8c84e4f0761f069e9a99e869ad202aad4e10
aryankohli2002 <73402480+aryankohli2002@users.noreply.github.com>
2024-04-04 12:57:24 +0000

    Dynamic pages for each job

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, text
 import os
 result_dict=[]
 db_connection_string = os.environ['DB_CONNECTION_STRING']
+engine = create_engine(db_connection_string)
 
 def load_jobs_from_db():
-  engine = create_engine(db_connection_string)
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     for row in result.all():
@@ -12,8 +12,14 @@ def load_jobs_from_db():
       
   return result_dict
   
-  
-
+def load_job_from_db_with_id(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs where id=:val"), {"val": id})
+    row = result.all()
+    if len(row) == 0:
+      return None
+    else:
+      return row[0]._asdict()
 # # Construct the connection URL
 # connection_url = f"mysql+mysqldb://aryan@/jovian?unix_socket=/cloudsql/trim-silicon-419117:demo-sql"
 
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template,jsonify
-from database import load_jobs_from_db
-app = Flask(__name__) #creating an object of the class Flask
-  
-@app.route("/") 
-#creating a function 
+from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db, load_job_from_db_with_id
+
+app = Flask(__name__)  # creating an object of the class Flask
+
+@app.route("/")
 def hello_world():
-    return render_template('home.html', jobs=load_jobs_from_db())
-  
+      return render_template('home.html', jobs=load_jobs_from_db())
+
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(load_jobs_from_db())
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db_with_id(id)  
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
+    
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug = True)
+  app.run(host='0.0.0.0', port=8080, debug=True)
